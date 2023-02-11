a5a82c774a10a5aad7fd79c539746cee826b725b
Pierre Lavergne <pierre.lavergne@epitech.eu>
2023-02-11 23:01:59 +0100

    add: Player which move with a map

---
+++ SuperVivor/Assets/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        animator.SetBool("isMoving", false);
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetBool("isMoving", true);
+            transform.Translate(Vector2.right * velocity * Time.deltaTime);
+            transform.localScale = new Vector3(1, 1, 1) * 0.3f;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            animator.SetBool("isMoving", true);
+            transform.Translate(Vector2.left * velocity * Time.deltaTime);
+            transform.localScale= new Vector3(-1, 1, 1) * 0.3f;
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            animator.SetBool("isMoving", true); ;
+            transform.Translate(Vector2.up * velocity * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("isMoving", true);
+            transform.Translate(Vector2.down * velocity * Time.deltaTime);
+        }
+    }
+
+    public float velocity = 3.0f;
+    public Animator animator;
+}
