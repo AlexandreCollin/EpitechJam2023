d16d9d60f0f6cc921f2563e04ac3d33923539925
Alexandre Collin <alexandre.collin-betheuil@epitech.eu>
2023-02-12 18:02:58 +0100

    fix: if player go to both x and y axis at the same time, keep the same speed

--- SuperVivor/Assets/Movement.cs
+++ SuperVivor/Assets/Movement.cs
@@ -26,17 +26,29 @@ public class Movement : MonoBehaviour
         {
             animator.SetBool("isMoving", true);
             transform.Translate(Vector2.left * velocity * Time.deltaTime);
-            transform.localScale= new Vector3(-1, 1, 1) * scale;
+            transform.localScale = new Vector3(-1, 1, 1) * scale;
         }
         if (Input.GetKey(KeyCode.Z))
         {
             animator.SetBool("isMoving", true);
-            transform.Translate(Vector2.up * velocity * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.up * velocity / 2 * Time.deltaTime);
+            } else {
+                transform.Translate(Vector2.up * velocity * Time.deltaTime);
+            }
         }
         if (Input.GetKey(KeyCode.S))
         {
             animator.SetBool("isMoving", true);
-            transform.Translate(Vector2.down * velocity * Time.deltaTime);
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.down * velocity / 2 * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector2.down * velocity * Time.deltaTime);
+            }
         }
     }
 }
